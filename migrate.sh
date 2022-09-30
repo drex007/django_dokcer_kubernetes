78245433f638223a352f993f084e716a2ef2ea1c
drex007 <vykeoj@gmail.com>
2022-09-30 17:01:31 +0100

    feat: added docker compose file

--- migrate.sh
+++ migrate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"viktor@gmail.com"}
 
-
-
 cd /app/  #We went into the directory where we can locate our manage.py file
-/opt/env/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL -- noinput
+
+
+/opt/env/bin/python manage.py migrate --noinput
+/opt/env/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL -- noinput || true

3749d9be057523b88afaf7514b20c37a73c2814f
drex007 <vykeoj@gmail.com>
2022-10-01 15:04:40 +0100

    feat:creating superuser

--- migrate.sh
+++ migrate.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"viktor@gmail.com"}
+USERNAME=${DJANGO_SUPERUSER_USERNAME}
 
 cd /app/  #We went into the directory where we can locate our manage.py file
 
 
 /opt/env/bin/python manage.py migrate --noinput
-/opt/env/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL -- noinput || true
+/opt/env/bin/python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $SUPERUSER_EMAIL
